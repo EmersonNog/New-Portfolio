db5e1d213dd9bafd83253e517368a33ffe4f5055
EmersonNog <emersonnogueira54321@gmail.com>
2022-10-22 11:48:43 -0300

    add screen loading

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function showLoading() {
+    let display = document.getElementById('tela')
+    let body = document.body
+    display.style.display = 'none'
+    body.style.overflow = 'visible'
+}
+
+setTimeout(showLoading, 5000)
